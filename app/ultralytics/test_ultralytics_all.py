99518a9c51c2d2e7bda8101ce3df6564749a73e3
the-puzzler <172930174+the-puzzler@users.noreply.github.com>
2025-09-20 00:01:52 +0200

    added video tests

--- app/ultralytics/test_ultralytics_all.py
+++ app/ultralytics/test_ultralytics_all.py
@@ -6,8 +6,17 @@ model = YOLO("yolov8m-oiv7.pt")
 # Option B: load directly from a URL (Ultralytics can fetch remote .pt)
 # model = YOLO("https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8m-oiv7.pt")
 
-results = model("test_images/IMG_7679.jpg", conf=0.25)
-results[0].show()
+# results = model("test_images/IMG_7679.jpg", conf=0.25)
+# results[0].show()
+
+model.predict(
+    source="test_videos/test_video.mp4",  
+    conf=0.25,
+    save=True,
+    show = True,
+    project="runs/detect",
+    name="oiv7_video"
+)
 
 # See the ~600 class names
 print(len(model.names), "classes")
